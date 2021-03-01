79e8090d9d0105942d9bf9f87ef96c16e8b81a3e
Cyberhan123 <255542417@qq.com>
2021-03-01 10:37:45 +0800

    增加session，待完成redis

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Application } from "oak/mod.ts";
+import { Application, helpers } from "oak/mod.ts";
 import { ErrorInterceptor } from "@/middleware/mod.ts";
 import router from "@/routers/mod.ts";
 import config from "./config/mod.ts";
@@ -7,7 +7,7 @@ import session from "@/helper/session/mod.ts";
 import logger from "@/helper/logger/mod.ts";
 
 const app = new Application();
-logger.debug("pace logger start working...")
+logger.debug("pace logger start working...");
 app.keys = config.keys;
 app.addEventListener("listen", ({ hostname, port, secure }) => {
   Deno.readTextFile("./src/banner.txt").then((response: any) =>
@@ -24,21 +24,57 @@ app.use(errorInsterceptor.httpErrorInterceptor());
 app.use(
   session({
     // store: redisStore(config.redis),
-    cookie:{
-      path: '/',
+    cookie: {
+      path: "/",
       httpOnly: false,
       maxAge: 24 * 60 * 60 * 1000, //one day in ms,
       signed: true,
+    },
+  }),
+);
+app.use(async (ctx: Record<string, any>, next) => {
+  await next();
+  if (
+    typeof ctx.request.body === "object" &&
+    ctx.request.body()?.data !== undefined
+  ) {
+    ctx.response.type = "json";
+    ctx.response.body = JSON.stringify(ctx.response.body, undefined, 2);
+  }
+});
+//增加query对象
+app.use(async (ctx, next) => {
+  const query = helpers.getQuery(ctx, { mergeParams: true });
+  Object.defineProperties(ctx.request, {
+    "query": {
+      value: query,
+      writable: false,
+    },
+  });
+  Object.defineProperties(ctx,{
+    "query":{
+      value: query,
+      writable: false,
     }
   })
-)
-// app.use(logger.logger);
-// app.use(logger.responseTime);
-
+  await next();
+});
+//jsonp
+app.use(async (ctx: Record<string, any>, next) => {
+  logger.debug(`当前ctx为${JSON.stringify(ctx.request)}`);
+  await next();
+  logger.debug(`当前ctx为${JSON.stringify(ctx.request)}`);
+  if (ctx.request.query?.callback) {
+    let body = typeof ctx.response.body === "object"
+      ? JSON.stringify(ctx.body, undefined, 2)
+      : ctx.response.body;
+    ctx.response.body = ctx.request.query.callback + "(" + body + ")";
+    ctx.response.type = "application/x-javascript";
+  }
+});
 
 app.use(router.routes());
 app.use(router.allowedMethods());
-
 
 await app.listen({
   port: config.serve.port,
--- src/routers/logGather.ts
+++ src/routers/logGather.ts
@@ -4,17 +4,17 @@ router
   //hello world
 
   // 错误上传
-  .post("/error", (context) => {
+  .post("/logger", (ctx: any) => {
     //标签，请求的接口，页面url， log日志，时间
     // const{tag,request,url,log,time} = context.request.body;
 
-    context.response.body = "error"
+    ctx.response.body = "error";
   })
-  .delete("/error",(context)=>{
+  .delete("/logger", (ctx: any) => {
     //error 根据 id 删除
     // const{id} = context.request.body;
   })
   //通过时间查询
-  .get("/error/:time", (context) => {
-    context.response.body = "error/:time"
+  .get("/logger/:time", (ctx: { response: { body: string } }) => {
+    ctx.response.body = "error/:time";
   });
